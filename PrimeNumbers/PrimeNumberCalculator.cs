6fd0ffe95f5b39f295f4d155f69220d7dbf798e0
heather dougherty <heatherdougherty1234@me.com>
2020-12-24 00:51:36 -0500

    implemented PrimeNumberCalculator method

--- PrimeNumbers/PrimeNumberCalculator.cs
+++ PrimeNumbers/PrimeNumberCalculator.cs
@@ -1,15 +1,31 @@
-﻿using System;
-
-namespace PrimeNumbers
+﻿namespace PrimeNumbers
 {
-	public class PrimeNumberCalculator
-	{
-		/// <summary>
-		/// Returns true if the value argument is a prime number, otherwise returns false.
-		/// </summary>
-		public bool IsPrime(int value)
-		{
-			throw new NotImplementedException();
-		}
-	}
+  public class PrimeNumberCalculator
+  {
+    /// <summary>
+    /// Returns true if the value argument is a prime number, otherwise returns false.
+    /// </summary>
+    public bool IsPrime(int value)
+    {
+      if (value < 0)
+      {
+        value = value * -1;
+      }
+
+      if (value == 0 || value == 1)
+      {
+        return false;
+      }
+
+      for (int i = 2; i <= value / 2; i++)
+      {
+        if (value % i == 0)
+        {
+          return false;
+        }
+      }
+
+      return true;
+    }
+  }
 }
